8a30a33c810ab8d3eb695fb9f872a6ce53907f63
zhenye <zhenye.ding@foxmail.com>
2022-06-08 16:32:05 +0800

    添加actautor

--- feign/feign-producer/src/main/java/io/zhenye/feign/producer/controller/Controller.java
+++ feign/feign-producer/src/main/java/io/zhenye/feign/producer/controller/Controller.java
@@ -1,21 +1,25 @@
 package io.zhenye.feign.producer.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RestController
 public class Controller {
 
     @GetMapping("/user/{id}")
     public String get(@PathVariable("id") Integer id) {
+        log.info("dispose id[{}]", id);
         return "success";
     }
 
     @GetMapping("/timeout/{milliSeconds}")
     public String timeout(@PathVariable("milliSeconds") Integer milliSeconds) throws InterruptedException {
+        log.info("dispose timeout[{}ms]", milliSeconds);
         TimeUnit.MILLISECONDS.sleep(milliSeconds);
         return "success";
     }
